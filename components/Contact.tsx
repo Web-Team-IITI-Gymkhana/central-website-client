8c120fc2bce9da48673304fc0dc937a7d6881e8b
Devesh <deveshlokare1968@gmail.com>
2024-01-04 19:46:51 +0530

    location

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -41,12 +41,12 @@ const contact = () => {
               <p className='ml-4  font-[Inter] font-semibold max-[280px]:text-sm'>{email}</p>
             </div>
             <div className='flex flex-column mt-2 mb-4 '>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 min-w-[1.5rem] min-h-[1.5rem]">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
               </svg>
 
-              <p className=' ml-4 font-[Inter] font-semibold  max-[280px]:text-sm'>{address}</p>
+              <p className=' ml-4 font-[Inter] font-semibold max-[280px]:text-sm'>{address}</p>
             </div>
           </div>
 
